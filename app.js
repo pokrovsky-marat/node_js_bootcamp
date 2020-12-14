61646a41389928b4db9070f40a250d1e89c15a13
Marat Isaev <fake@mail.com>
2020-12-14 21:01:41 +0600

    57 lesson routing, refactoring code

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const tours = JSON.parse(
     fs.readFileSync("./dev-data/data/tours-simple.json", "utf-8")
 );
 app.use(express.json());
-app.get("/api/v1/tours", (req, res) => {
+
+const getTours = (req, res) => {
     res
         .status(200)
         .json({status: "success", results: tours.length, data: {tours}});
-});
-app.get("/api/v1/tours/:id", (req, res) => {
+}
+const getTour = (req, res) => {
     let id = req.params.id;
     if (id > tours.length) {
         return res.status(404).json({status: "fail", message: "Invalid ID"});
@@ -20,9 +21,8 @@ app.get("/api/v1/tours/:id", (req, res) => {
         return el.id == id; //id is a string === is not equal
     });
     res.status(200).json({status: "success", data: {tour}});
-});
-
-app.post("/api/v1/tours", (req, res) => {
+}
+const createTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newId}, req.body);
     tours.push(newTour);
@@ -38,25 +38,32 @@ app.post("/api/v1/tours", (req, res) => {
             });
         }
     );
-});
-app.patch('/api/v1/tours/:id', (req, res) => {
+}
+const updateTour = (req, res) => {
     console.log(req.body)
     res.json({
         status: "success",
         message: 'Ok'
     })
-});
-app.delete("/api/v1/tours/:id", (req, res) => {
-
+}
+const deleteTour = (req, res) => {
     res.status(204).json({
         status: "success",
         data: {
             message: "Ok ",
         },
     });
+}
 
+/*app.get("/api/v1/tours", getTours);
+app.post("/api/v1/tours", createTour);
 
-});
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete("/api/v1/tours/:id", deleteTour);
+app.get("/api/v1/tours/:id", getTour);*/
+
+app.route("/api/v1/tours").get(getTours).post(createTour);
+app.route('/api/v1/tours/:id').patch(updateTour).delete(deleteTour).get(getTour);
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
