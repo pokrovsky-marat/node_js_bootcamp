d51b701a4f5c4a0c9e0ba064c138fc43061d9045
Marat Isaev <fake@mail.com>
2020-12-15 12:00:55 +0600

    lesson 63 refactoring code, decomposition

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
-const fs = require("fs");
+const userRouter = require('./routes/userRouter')
+const tourRouter = require('./routes/tourRouter')
 
 const app = express();
-const tours = JSON.parse(
-    fs.readFileSync("./dev-data/data/tours-simple.json", "utf-8")
-);
 
 //1) Middleware
 app.use(morgan("dev"))
@@ -20,97 +18,10 @@ app.use((req, res, next) => {
     next();
 });
 
-//2) Route handlers
-const getTours = (req, res) => {
-    res
-        .status(200)
-        .json({status: "success", results: tours.length, data: {tours}});
-}
-const getTour = (req, res) => {
-    let id = req.params.id;
-    if (id > tours.length) {
-        return res.status(404).json({status: "fail", message: "Invalid ID"});
-    }
-    let tour = tours.find((el) => {
-        return el.id == id; //id is a string === is not equal
-    });
-    res.status(200).json({status: "success", time: req.requestedTime, data: {tour}});
-}
-const createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-    tours.push(newTour);
-    fs.writeFile(
-        "./dev-data/data/tours-simple.json",
-        JSON.stringify(tours),
-        (err) => {
-            res.status(201).json({
-                status: "success",
-                data: {
-                    tour: newTour,
-                },
-            });
-        }
-    );
-}
-const updateTour = (req, res) => {
-    console.log(req.body)
-    res.json({
-        status: "success",
-        message: 'Ok'
-    })
-}
-const deleteTour = (req, res) => {
-    res.status(204).json({
-        status: "success",
-        data: {
-            message: "Ok ",
-        },
-    });
-}
-
-//2.2) Route handlers for users
-const getAllUsers = (req, res) => {
-    res
-        .status(500)
-        .json({status: "error", message: "This route has not yet defined"});
-}
-const getUser = (req, res) => {
-    res
-        .status(500)
-        .json({status: "error", message: "This route has not yet defined"});
-}
-const createUser = (req, res) => {
-    res
-        .status(500)
-        .json({status: "error", message: "This route has not yet defined"});
-}
-const updateUser = (req, res) => {
-    res
-        .status(500)
-        .json({status: "error", message: "This route has not yet defined"});
-}
-const deleteUser = (req, res) => {
-    res
-        .status(500)
-        .json({status: "error", message: "This route has not yet defined"});
-}
 
 //3) Routes
-
-const userRouter = express.Router();
-const tourRouter = express.Router();
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 
-tourRouter.route("/").get(getTours).post(createTour);
-tourRouter.route('/:id').patch(updateTour).delete(deleteTour).get(getTour);
 
-userRouter.route("/").get(getAllUsers).post(createUser);
-userRouter.route('/:id').patch(updateUser).delete(deleteUser).get(getUser);
-
-//4) Start the server
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-});
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const getAllUsers = (req, res) => {
+    res
+        .status(500)
+        .json({status: "error", message: "This route has not yet defined"});
+}
+const getUser = (req, res) => {
+    res
+        .status(500)
+        .json({status: "error", message: "This route has not yet defined"});
+}
+const createUser = (req, res) => {
+    res
+        .status(500)
+        .json({status: "error", message: "This route has not yet defined"});
+}
+const updateUser = (req, res) => {
+    res
+        .status(500)
+        .json({status: "error", message: "This route has not yet defined"});
+}
+const deleteUser = (req, res) => {
+    res
+        .status(500)
+        .json({status: "error", message: "This route has not yet defined"});
+}
+
+module.exports = {getAllUsers, getUser, createUser, updateUser, deleteUser}
