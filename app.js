6167a686528388490ec52e223eca26c4d22d9039
Marat Isaev <fake@mail.com>
2021-01-09 10:45:38 +0600

    #114, Custom Error Class and ErrorHandler are separated in separate files

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const userRouter = require("./routes/userRouter");
 const tourRouter = require("./routes/tourRouter");
-
+const AppError = require("./utils/AppError");
+const globalErrorController = require("./controllers/globalErrorController");
 const app = express();
 
 //1) Middleware
@@ -21,19 +22,9 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 //4)Handling With Unhandled Routes
 app.all("*", (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server.`);
-  err.statusCode = 404;
-  err.status = "fail";
-  next(err); //Pass "err" to the next middlware function
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404)); //Pass "err" to the next middlware function
 });
 
 //Global Error Handling
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 404;
-  err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorController);
 module.exports = app;
