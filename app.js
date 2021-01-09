30d78923b9fa16dc88234e6801a34214e39edf15
Marat Isaev <fake@mail.com>
2021-01-09 10:09:14 +0600

    #113, Global Error Handling Middlware

--- app.js
+++ app.js
@@ -21,9 +21,19 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 //4)Handling With Unhandled Routes
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server.`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server.`);
+  err.statusCode = 404;
+  err.status = "fail";
+  next(err); //Pass "err" to the next middlware function
+});
+
+//Global Error Handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 404;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 module.exports = app;
