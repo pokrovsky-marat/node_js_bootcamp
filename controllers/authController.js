edae3423df0cd7c01213433c6c201da16c0df483
Marat Isaev <fake@mail.com>
2021-01-13 22:49:22 +0600

    #128, jwt.sign, token, signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,16 @@
 const User = require("./../models/userModel");
 const cathAsyncErrors = require("./../utils/cathAsyncErrors");
+const jwt = require("jsonwebtoken");
 
 exports.signup = cathAsyncErrors(async (req, res, next) => {
   const newUser = await User.create(req.body);
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: "success",
-    body: {
+    token,
+    data: {
       user: newUser,
     },
   });
