343775565d98847ebeb2a3966d0e8c180fbaf8a2
Marat Isaev <fake@mail.com>
2021-01-17 09:39:53 +0600

    141 Sending JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,15 @@ const makeToken = (id) => {
 
 const makeTokenAndSend = (user, res, statusCode) => {
   const token = makeToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  res.cookie("jwt", token, cookieOptions);
+  user.password = undefined; //Don't show password to client
   res.status(statusCode).json({
     status: "success",
     token,
